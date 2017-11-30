d42b67330bb45a30ed6e2cc93f4317bc4b289ff8
James McCutcheon <james_mccutcheon@hotmail.com>
2017-11-29 23:48:58 -0800

    Update

--- ui.R
+++ ui.R
@@ -4,30 +4,21 @@ library(leaflet)
 
 source("data.R")
 
-ui <- fluidPage(navbarPage(
-  "Zillow Housing Data",
-  tabPanel("State Data",
-           sidebarLayout(
-             sidebarPanel(
-               selectInput(
-                 "states",
-                 label = "Select Your Desired State(s)",
-                 choices = states$AREA,
-                 multiple = T,
-                 selected = "Washington"
-               ),
-               
-               sliderInput(
-                 "range",
-                 label = "Select a Year Range",
-                 sep = "",
-                 min = 1996,
-                 max = as.integer(format(Sys.Date(), "%Y")) - 1,
-                 value = c(1996, as.integer(format(Sys.Date(
-                 ), "%Y")) - 1)
-               )
+ui <- fluidPage(
+  navbarPage("Zillow Housing Data",
+             
+             tabPanel("State Data",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("states", label = "Select Your Desired State(s)", choices = states$AREA, multiple = T, selected = "Washington"),
+                          sliderInput("range", label = "Select a Year Range", sep = "", min = 1996, max = as.integer(format(Sys.Date(), "%Y")) - 1, 
+                                      value = c(1996, as.integer(format(Sys.Date(), "%Y")) - 1))
+                        ),
+                        mainPanel(
+                          plotlyOutput("state.plot")                       
+                          )
+                      )
              ),
-<<<<<<< HEAD
              tabPanel("City Data",
                       sidebarLayout(
                         sidebarPanel(
@@ -39,25 +30,6 @@ ui <- fluidPage(navbarPage(
                           plotlyOutput("city.rental.plot")
                         )
                       )
-=======
-             mainPanel(plotlyOutput("state.plot"))
-           )),
-  
-  tabPanel("City Data",
-           sidebarLayout(
-             sidebarPanel(
-               selectInput(
-                 "cities",
-                 label = "Select Your Desired City/Cities",
-                 choices = wa.cities$city,
-                 multiple = T,
-                 selected = "Seattle"
-               )
-             ),
-             mainPanel(
-               plotlyOutput("city.sale.plot"),
-               plotlyOutput("city.rental.plot")
->>>>>>> 251ee9fd97b4b5ec84946c27cb73c238e990241d
              )
-           ))
-))
+  )
+)
