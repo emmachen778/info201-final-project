189c94f7589547d5887dc577144360aff947308a
emilytao19 <emilytao@uw.edu>
2017-12-02 16:58:37 -0800

    finished predicted prices tab, close #2

--- ui.R
+++ ui.R
@@ -5,91 +5,65 @@ library(shinythemes)
 
 source("data.R")
 
-ui <- fluidPage(
-  shinythemes::themeSelector(),
-  navbarPage(
-    "Zillow Housing Data",
-    tabPanel("Home",
-             fluidRow(
-               column(
-                 width = 3,
-                 offset = 1,
-                 includeMarkdown("quandl.md")
-               ),
-               column(7, includeMarkdown("welcome.md"))
-             )),
-    
-    tabPanel("State Data",
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput(
-                   "states",
-                   label = "Select Your Desired State(s)",
-                   choices = states$AREA,
-                   multiple = T,
-                   selected = "Washington"
-                 ),
-                 sliderInput(
-                   "range",
-                   label = "Select a Year Range",
-                   sep = "",
-                   min = 1996,
-                   max = as.integer(format(Sys.Date(), "%Y")) - 1,
-                   value = c(1996, as.integer(format(Sys.Date(
-                   ), "%Y")) - 1)
+ui <- fluidPage(shinythemes::themeSelector(),
+  navbarPage("Zillow Housing Data",
+             tabPanel("Home",
+                      fluidRow(
+                        column(width = 3, offset = 1, includeMarkdown("quandl.md")),
+                        column(7, includeMarkdown("welcome.md"))
+                      )
+             ),
+             
+             tabPanel("State Data",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("states", label = "Select Your Desired State(s)", choices = states$AREA, multiple = T, selected = "Washington"),
+                          sliderInput("range", label = "Select a Year Range", sep = "", min = 1996, max = as.integer(format(Sys.Date(), "%Y")) - 1, 
+                                      value = c(1996, as.integer(format(Sys.Date(), "%Y")) - 1))
+                        ),
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Sales", plotlyOutput("state.plot")), 
+                            tabPanel("Rentals", plotlyOutput("state.rent.plot"))
+                          )
+                        )
+                      )
+             ),
+             
+             tabPanel("City Data",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("cities", label = "Select Your Desired City/Cities", choices = wa.cities$city, multiple = T, selected = "Seattle")
+                        ),
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel(
+                              "Map", tags$h3("Change in Median Sale Prices"), leafletOutput("map"),
+                              tags$div(tags$br(),
+                                       tags$p("This map shows the location of the selected city/cities, indicated by a circle. 
+                                              The circle sizes are scaled relative to the percent change in median sale prices over time.")
+                                       
+                              )
+                              ),
+                            tabPanel("Sales", plotlyOutput("city.sale.plot")),
+                            tabPanel("Rentals", plotlyOutput("city.rental.plot"))
+                          )
+                        )
+                      )
+             ),
+             
+             tabPanel("Predicted Prices for a City",
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("city", label = "Select Your Desired City", choices = wa.cities$city, multiple = F, selected = "Seattle")
+                 ), 
+                 mainPanel(
+                   tabsetPanel(
+                      tabPanel("Sales", plotlyOutput("predict.sale")),
+                      tabPanel("Rentals", plotlyOutput("predict.rent"))
+                   )
                  )
-               ),
-               mainPanel(tabsetPanel(
-                 tabPanel("Sales", plotlyOutput("state.plot")),
-                 tabPanel("Rentals", plotlyOutput("state.rent.plot"))
-               ))
-             )),
-    
-    tabPanel("City Data",
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput(
-                   "cities",
-                   label = "Select Your Desired City/Cities",
-                   choices = wa.cities$city,
-                   multiple = T,
-                   selected = "Seattle"
-                 )
-               ),
-               mainPanel(tabsetPanel(
-                 tabPanel(
-                   "Map",
-                   tags$h3("Change in Median Sale Prices"),
-                   leafletOutput("map"),
-                   tags$div(
-                     tags$br(),
-                     tags$p(
-                       "This map shows the location of the selected city/cities, indicated by a circle.
-                       The circle sizes are scaled relative to the percent change in median sale prices over time."
-                     )
-                     
-                     )
-                 ),
-                 tabPanel("Sales", plotlyOutput("city.sale.plot")),
-                 tabPanel("Rentals", plotlyOutput("city.rental.plot"))
-                 ))
-               )),
-    
-    tabPanel(
-      "Predicted Prices for a City",
-      sidebarLayout(sidebarPanel(
-        selectInput(
-          "city",
-          label = "Select Your Desired City",
-          choices = wa.cities$city,
-          multiple = F,
-          selected = "Seattle"
-        )
-      ),
-      mainPanel(tabsetPanel(
-        tabPanel("Sales", plotlyOutput("predict.sale")),
-        tabPanel("Rentals", plotlyOutput("predict.rent"))
-      )))
-    )
+               )
+             )
   )
 )
